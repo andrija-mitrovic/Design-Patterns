3238e84c8a582c0c69a7c5c3d6920a92e38daa14
andrija-mitrovic <57568556+andrija-mitrovic@users.noreply.github.com>
2020-04-03 18:09:15 +0200

    Singleton Pattern Examples

---
+++ Singleton Pattern/Example1/Example1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var db = SingletonDataContainer.Instance;
+            var db2 = SingletonDataContainer.Instance;
+            var db3 = SingletonDataContainer.Instance;
+            var db4 = SingletonDataContainer.Instance;
+
+            Console.ReadLine();
+        }
+    }
+}
