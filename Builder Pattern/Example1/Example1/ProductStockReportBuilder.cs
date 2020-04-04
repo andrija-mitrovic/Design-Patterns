68a3efb07990dbd9d042d1efbfd8eb470f683b84
andrija-mitrovic <57568556+andrija-mitrovic@users.noreply.github.com>
2020-04-04 18:13:58 +0200

    Builder Pattern Examples

---
+++ Builder Pattern/Example1/Example1/ProductStockReportBuilder.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example1
+{
+    public class ProductStockReportBuilder : IProductStockReportBuilder
+    {
+        private ProductStockReport _productStockReport;
+        private IEnumerable<Product> _products;
+
+        public ProductStockReportBuilder(IEnumerable<Product> products)
+        {
+            _products = products;
+            _productStockReport = new ProductStockReport();
+        }
+
+        public void BuildBody()
+            => _productStockReport.HeaderPart = $"STOCK REPORT FOR ALL THE PRODUCTS ON DATE: {DateTime.Now}\n";
+
+        public void BuildFooter()
+            => _productStockReport.BodyPart= string.Join(Environment.NewLine, _products.Select(p => $"Product name: {p.Name}, product price: {p.Price}"));
+
+        public void BuildHeader()
+            => _productStockReport.FooterPart = "\n Report provided by the IT_PRODUCTS company.";
+
+        public ProductStockReport GetReport()
+        {
+            var productStockReport = _productStockReport;
+            Clear();
+            return productStockReport;
+        }
+
+        private void Clear() => _productStockReport = new ProductStockReport();
+    }
+}
