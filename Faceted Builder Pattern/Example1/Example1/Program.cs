90e7d94145c254017baa128912804113ef31ff7f
andrija-mitrovic <57568556+andrija-mitrovic@users.noreply.github.com>
2020-04-04 18:29:40 +0200

    Faceted Builder Pattern Example

---
+++ Faceted Builder Pattern/Example1/Example1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Example1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var car = new CarBuilderFacade()
+            .Info
+              .WithType("BMW")
+              .WithColor("Black")
+              .WithNumberOfDoors(5)
+            .Built
+              .InCity("Leipzig ")
+              .AtAddress("Some address 254")
+            .Build();
+
+            Console.WriteLine(car);
+
+            Console.ReadLine();
+        }
+    }
+}
